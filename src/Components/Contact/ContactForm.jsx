9835742ec48423bc788fca7f4b4e4cacfea3048b
Kieran Best <best.kieran.4595@gmail.com>
2024-10-24 21:23:18 +0100

    Carry email over to next page

--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -1,29 +1,21 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 import { getImageUrl } from "../../utils";
 
 export const ContactForm = () => {
+
+    const location = useLocation();
+    const data = location.state;
+
+    console.log(data)
     return (
         <section className="z-0" id="intro">
             <div className="grid grid-cols-1 sm:grid-cols-2 px-20 text-center py-20 gap-16">
                 <div className=" bg-slate-400 p-6 rounded-lg">
-                    <h1>Family Run</h1>
+                    <p>{ useLocation().state }</p>
                 </div>
-                <div className="bg-slate-400 p-6 rounded-lg">
-                    <h1>Quality Wood</h1>
-                </div>
-                <div className="bg-slate-400 p-6 rounded-lg">
-                    <img src={getImageUrl("logs.jpg")} alt="placeholder" className="mx-auto rounded-lg" />
-                </div>
-                <div className="bg-slate-400 p-6 rounded-lg">
-                    <img src={getImageUrl("kindling.jpg")} alt="placeholder" className="mx-auto rounded-lg" />
-                </div>
-                <div className="bg-slate-400 p-6 rounded-lg">
-                    <h1>Professionally Dried</h1>
-                </div>
-                <div className="bg-slate-400 p-6 rounded-lg">
-                    <h1>Logs and Kindling</h1>
-                </div>
+
             </div>
         </section>
     );
--- src/Components/Home/Contact.jsx
+++ src/Components/Home/Contact.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import contacts from "../../Data/Contacts.json"
 
 export const Contact = () => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+
+    function handleClick(email) {
+        setEmail(email);
+        navigate("/contact", { state: email } )
+    }
+
     return (
         <section className="z-0" id="intro">
             <div className="px-20 text-center py-10">
@@ -13,12 +22,14 @@ export const Contact = () => {
                             <div className="p-5">
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{contacts.title}</h5>
                                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{contacts.description}</p>
-                                <a href="#" className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <a onClick={() => handleClick(contacts.email)}
+                                    className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     Contact us!
                                     <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                                     </svg>
                                 </a>
+                                <p>You clicked {email}</p>
                             </div>
                         </div>
                     ))}
